6ef866026878596944522dca3d15a37e62b978b3
camj10 <jiselcaballero06@gmail.com>
2025-01-25 15:03:29 -0300

    Update db file

--- server/config/db/db.js
+++ server/config/db/db.js
@@ -2,11 +2,12 @@ import mysql from 'mysql2/promise';
 
 const connection = async () => {
   try {
-    const db = await mysql.createPool({
+    const db = mysql.createPool({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      waitForConnections: true
     });
     console.log(`DB connected`);
     return db;  
